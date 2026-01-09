ead611f6e1e19b6901dd510c03b90f11fb5b0734
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-09 12:09:04 +0800

    fix

--- parallel_agent/eval/gen_gsm_infinite.py
+++ parallel_agent/eval/gen_gsm_infinite.py
@@ -59,7 +59,8 @@ for is_tail in [False, True]:
                 current_filter = {key: value for key, value in config.items() if key not in ["percentage"]}
                 filtered_subset = dataset_split.filter(lambda example: all(example[key] == value for key, value in current_filter.items()))
                 # naive split by head and tail
-                assert num_to_add * 2 <= len(filtered_subset), f"num_to_add: {num_to_add}  len(filtered_subset): {len(filtered_subset)}"
+                if length_str in ["8K", "16K", "32K"]: # both in train and test
+                    assert num_to_add * 2 <= len(filtered_subset), f"num_to_add: {num_to_add}  len(filtered_subset): {len(filtered_subset)}"
                 if is_tail:
                     filtered_data.extend(filtered_subset.select(range(len(filtered_subset) - num_to_add, len(filtered_subset))))
                 else:
