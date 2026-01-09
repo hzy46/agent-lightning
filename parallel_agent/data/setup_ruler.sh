2b418e3a41e895e142e6426eb5a567241c93f558
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-09 18:36:01 +0800

    fix

--- parallel_agent/data/setup_ruler.sh
+++ parallel_agent/data/setup_ruler.sh
@@ -1,3 +1,6 @@
 sudo apt update
 sudo apt install -y aria2
 bash hfd.sh BytedTsinghua-SIA/hotpotqa --dataset --tool aria2c -x 10 --local-dir ~/ruler_from_memagent
+
+python gen_gsm_infinite.py 
+python gen_ruler_mini.py 
