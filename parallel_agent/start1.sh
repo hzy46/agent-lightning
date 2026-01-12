22081bf2239e469e03e02a96783c6e06b60554ed
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-12 15:48:31 +0800

    fix

--- parallel_agent/start1.sh
+++ parallel_agent/start1.sh
@@ -10,4 +10,4 @@ env NCCL_CUMEM_HOST_ENABLE=0 \
     # NCCL_P2P_DISABLE=1 \
     # NCCL_CUMEM_HOST_ENABLE=0 \
 
-python train.py --train_doc_nums "[]" --train_gsm_lengths "['8K', '16K']"
+python train.py --method normal
