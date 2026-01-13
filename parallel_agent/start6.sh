953b002dbd300535972b6c52426ff3a13105371c
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-13 11:11:05 +0800

    fix

---
+++ parallel_agent/start6.sh
@@ -0,0 +1,9 @@
+ray stop
+
+export NCCL_CUMEM_HOST_ENABLE=0
+
+env NCCL_CUMEM_HOST_ENABLE=0 \
+    WANDB_API_KEY=75b560b94c4e949455fa45b3a018987ede3846fa \
+    RAY_DEBUG=legacy HYDRA_FULL_ERROR=1 VLLM_USE_V1=1 ray start --head --dashboard-host=0.0.0.0
+
+python train.py --method normal
