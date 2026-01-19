7be421e2d72a3b00fdac5b5eb966036542bd69c1
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-19 16:47:28 +0800

    fix

--- parallel_agent/start2.sh
+++ parallel_agent/start2.sh
@@ -10,4 +10,4 @@ env NCCL_CUMEM_HOST_ENABLE=0 \
     # NCCL_P2P_DISABLE=1 \
     # NCCL_CUMEM_HOST_ENABLE=0 \
 
-python train.py --method parallel --train_gsm_lengths "['16K']" --use_token_penalty True --token_penalty_L 2048 --token_penalty_k 0.0001
+python train.py --method parallel --train_gsm_lengths "['16K']" --fix_chunk_num 2
