86381008351de1e57588764d88c03f7cf13a87af
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-22 11:04:28 +0800

    fix

--- parallel_agent/start2.sh
+++ parallel_agent/start2.sh
@@ -10,4 +10,6 @@ env NCCL_CUMEM_HOST_ENABLE=0 \
     # NCCL_P2P_DISABLE=1 \
     # NCCL_CUMEM_HOST_ENABLE=0 \
 
-python train.py --method parallel --train_gsm_lengths "['16K']" --fix_chunk_num 2
+# python train.py --method parallel --train_gsm_lengths "['16K']" --fix_chunk_num 2
+python train.py --method stream --train_gsm_lengths "['16K']" --fix_chunk_num 2 --agg_mode True --use_new_gen_data True
+
