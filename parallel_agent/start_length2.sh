8f0fb32ecd478b4693fd22ff2605bcabab4c4c35
Zhiyuan He <zhiyuhe@microsoft.com>
2026-01-25 20:06:37 +0800

    fix

---
+++ parallel_agent/start_length2.sh
@@ -0,0 +1,16 @@
+ray stop
+
+export NCCL_CUMEM_HOST_ENABLE=0
+
+env NCCL_CUMEM_HOST_ENABLE=0 \
+    WANDB_API_KEY=75b560b94c4e949455fa45b3a018987ede3846fa \
+    RAY_DEBUG=legacy HYDRA_FULL_ERROR=1 VLLM_USE_V1=1 ray start --head --dashboard-host=0.0.0.0
+
+# env NCCL_IGNORE_DISABLED_P2P=1 \
+    # NCCL_P2P_DISABLE=1 \
+    # NCCL_CUMEM_HOST_ENABLE=0 \
+
+python train.py --method stream --train_gsm_lengths "['16K']" --fix_chunk_num 2 --agg_mode True \
+    --use_token_penalty True \
+    --token_penalty_L 3072 \
+    --token_penalty_k 0.0001
