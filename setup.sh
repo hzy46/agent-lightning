faf017f100744c84d6dfb40fed9b004916310fff
Zhiyuan He <zhiyuhe@microsoft.com>
2026-02-03 22:32:38 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -64,5 +64,6 @@ python -m ipykernel install --user --name agl --display-name agl
 
 source ~/azureml_job_env.sh
 # cp -r $ZHIYUHE/Qwen2.5-7B-Instruct-Yarn/ ~
-cp -r $ZHIYUHE/models/train_qwen2.5-7b_normal_gsm_8K ~
-cp -r $ZHIYUHE/new_gen_gsm_infinite_parsed ~
+# cp -r $ZHIYUHE/models/train_qwen2.5-7b_normal_gsm_8K ~
+# cp -r $ZHIYUHE/new_gen_gsm_infinite_parsed ~
+cp -r $ZHIYUHE/qa_eval_more ~
